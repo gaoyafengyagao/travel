f72dddc268ce5f282516a8b9108c9a189a1657a5
高亚峰 <1970178916@qq.com>
2019-05-22 20:37:18 +0800

    add recommend

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,6 +4,7 @@
     <home-header></home-header>
     <HomeSwiper></HomeSwiper>
     <HomeIcon></HomeIcon>
+    <home-recommend></home-recommend>
     </div>
 </template>
 
@@ -11,12 +12,14 @@
 import HomeHeader from './components/Header.vue'
 import HomeSwiper from './components/swiper.vue'
 import HomeIcon from './components/icon.vue'
+import HomeRecommend from './components/recommend.vue'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcon
+    HomeIcon,
+    HomeRecommend
   }
 }
 </script>
---
+++ src/pages/home/components/recommend.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="recommend">
+        <div class="recommend-title">推荐</div>
+        <div class="recommend-content"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="stylus" scoped>
+    
+</style>
