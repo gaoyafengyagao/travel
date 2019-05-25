dc86ab7e13735a5176538980768796c75053a87e
高亚峰 <1970178916@qq.com>
2019-05-25 11:25:53 +0800

    add city-search

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,13 +1,16 @@
 <template>
     <div>
-       <city-header></city-header> 
+       <city-header></city-header>
+       <CitySearch></CitySearch> 
     </div>
 </template>
 <script>
 import CityHeader from './components/Header.vue'
+import CitySearch from './components/Search.vue'
 export default {
     components: {
-        CityHeader
+        CityHeader,
+        CitySearch
     }
 }
 </script>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="search">
+       <input type="text" placeholder="请输入搜索城市或拼音" class="search-content"> 
+    </div>
+</template>
+<script>
+export default {
+    name: 'CitySearch'
+}
+</script>
+<style lang="stylus" scoped>
+    .search{
+        height: 0.72rem;
+        padding: 0.1rem;
+        background-color #eee;
+        .search-content {
+            box-sizing: border-box;
+            height: 0.62rem;
+            line-height: 0.62rem;
+            text-align: center;
+            border-radius: 0.06rem
+            width: 100%;
+            color: #666;
+            padding: 0 0.1rem;
+        }
+    }
+</style>
